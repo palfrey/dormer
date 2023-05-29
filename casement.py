9da32fae4f64080c6e6b0b6024542505e59d14c2
Tom Parker-Shemilt <palfrey@tevp.net>
2023-05-29 18:47:37 +0100

    Add testing

--- casement.py
+++ casement.py
@@ -1,5 +1,6 @@
 import hashlib
 from argparse import ArgumentParser
+from dataclasses import dataclass
 from pathlib import Path
 from sys import exit
 from typing import Dict, List, Optional, cast
@@ -8,18 +9,19 @@ import yaml
 from i3ipc import Connection
 from typing_extensions import TypedDict
 
-i3 = Connection()
 
-
+@dataclass
 class OutputReply:
     name: str
 
 
+@dataclass
 class CommandReply:
     success: bool
     error: Optional[str]
 
 
+@dataclass
 class WorkspaceReply:
     num: int
     output: str
@@ -27,27 +29,7 @@ class WorkspaceReply:
     visible: bool
 
 
-workspaces = cast(List[WorkspaceReply], i3.get_workspaces())
-outputs = cast(List[OutputReply], i3.get_outputs())
-
-parser = ArgumentParser()
-subparsers = parser.add_subparsers(title="command", dest="subparser", required=True)
-subparsers.add_parser("save")
-subparsers.add_parser("load")
-
-ns = parser.parse_args()
-
-output_names = sorted(
-    [output.name for output in outputs if not output.name.startswith("xroot")]
-)
-hasher = hashlib.sha256()
-hasher.update(";".join(output_names).encode("utf-8"))
-key = hasher.hexdigest()
-path = Path(f"~/.config/casement/{key}.yaml").expanduser()
-path.parent.mkdir(parents=True, exist_ok=True)
-
-
-def check_command(command: str):
+def check_command(i3: Connection, command: str):
     rets = cast(List[CommandReply], i3.command(command))
     for ret in rets:
         if not ret.success:
@@ -60,50 +42,77 @@ class Config(TypedDict):
     workspaces: Dict[int, str]
 
 
-if ns.subparser == "save":
-    data: Config = {"outputs": output_names, "workspaces": {}}
+def run(i3: Connection):
+    workspaces = cast(List[WorkspaceReply], i3.get_workspaces())
+    outputs = cast(List[OutputReply], i3.get_outputs())
 
-    for workspace in workspaces:
-        data["workspaces"][workspace.num] = workspace.output
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers(title="commands", dest="command", required=True)
+    subparsers.add_parser("save")
+    subparsers.add_parser("load")
 
-    print(data)
+    ns = parser.parse_args()
 
-    with path.open("w") as f:
-        yaml.safe_dump(data, f)
-
-    print(f"Saved to {path}")
-
-elif ns.subparser == "load":
-    if not path.exists():
-        print(f"No existing config for {','.join(output_names)}")
-        exit(-1)
-    with path.open() as f:
-        config = cast(Config, yaml.safe_load(f))
-
-    existing_workspaces = dict(
-        [(workspace.num, workspace.output) for workspace in workspaces]
+    output_names = sorted(
+        [output.name for output in outputs if not output.name.startswith("xroot")]
     )
-    focused_workspace = [
-        workspace.num for workspace in workspaces if workspace.focused
-    ][0]
-    visible_workspaces = [
-        workspace.num for workspace in workspaces if workspace.visible
-    ]
-    changes = False
-    for name, output in config["workspaces"].items():
-        if existing_workspaces[name] != output:
-            changes = True
-            for command in [f"workspace {name}", f"move workspace to output {output}"]:
-                check_command(command)
+    hasher = hashlib.sha256()
+    hasher.update(";".join(output_names).encode("utf-8"))
+    key = hasher.hexdigest()
+    path = Path(f"~/.config/casement/{key}.yaml").expanduser()
+    path.parent.mkdir(parents=True, exist_ok=True)
 
-    if changes:
-        for visible_workspace in visible_workspaces:
-            check_command(f"workspace {visible_workspace}")
-        check_command(f"workspace {focused_workspace}")
-        print("Workspaces reset")
-    else:
-        print("No changes necessary")
+    if ns.command == "save":
+        data: Config = {"outputs": output_names, "workspaces": {}}
 
-else:
-    print(f"Unknown command: {ns.subparser}")
-    exit(-1)
+        for workspace in workspaces:
+            data["workspaces"][workspace.num] = workspace.output
+
+        with path.open("w") as f:
+            yaml.safe_dump(data, f)
+
+        print(f"Saved to {path}")
+
+    elif ns.command == "load":
+        if not path.exists():
+            print(f"No existing config for {','.join(output_names)}")
+            exit(-1)
+        with path.open() as f:
+            config = cast(Config, yaml.safe_load(f))
+
+        existing_workspaces = dict(
+            [(workspace.num, workspace.output) for workspace in workspaces]
+        )
+        focused_workspace = [
+            workspace.num for workspace in workspaces if workspace.focused
+        ][0]
+        visible_workspaces = [
+            workspace.num for workspace in workspaces if workspace.visible
+        ]
+        changes = False
+        for name, output in config["workspaces"].items():
+            if existing_workspaces[name] != output:
+                changes = True
+                for command in [
+                    f"workspace {name}",
+                    f"move workspace to output {output}",
+                ]:
+                    check_command(i3, command)
+
+        if changes:
+            for visible_workspace in visible_workspaces:
+                if visible_workspace != focused_workspace:
+                    check_command(i3, f"workspace {visible_workspace}")
+            check_command(i3, f"workspace {focused_workspace}")
+            print("Workspaces reset")
+        else:
+            print("No changes necessary")
+
+    else:  # pragma: no cover - effectively impossible
+        print(f"Unknown command: {ns.subparser}")
+        exit(-1)
+
+
+if __name__ == "__main__":  # pragma: no cover - can't seem to mock actual I3
+    i3 = Connection()
+    run(i3)
---
+++ test_casement.py
@@ -0,0 +1,127 @@
+import os
+import sys
+from typing import List
+from unittest.mock import patch
+
+import pytest
+import yaml
+from pyfakefs.fake_filesystem import FakeFilesystem
+
+from casement import CommandReply, OutputReply, WorkspaceReply, run
+
+
+class MockI3:
+    def __init__(self) -> None:
+        self.commands_run: List[str] = []
+
+    def get_workspaces(self) -> List[WorkspaceReply]:
+        return [
+            WorkspaceReply(num=1, output="foo", focused=True, visible=True),
+            WorkspaceReply(num=2, output="foo", focused=False, visible=True),
+        ]
+
+    def get_outputs(self) -> List[OutputReply]:
+        return [OutputReply(name="foo")]
+
+    def command(self, cmd: str) -> List[CommandReply]:
+        self.commands_run.append(cmd)
+        return []
+
+
+path = "/home/bar/.config/casement/2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae.yaml"  # noqa: E501
+
+
+def test_no_args(capsys: pytest.CaptureFixture):
+    with patch.object(sys, "argv", ["casement"]):
+        with pytest.raises(SystemExit):
+            run(MockI3())
+
+    res = capsys.readouterr()
+    assert (
+        res.err.find("error: the following arguments are required: command") != -1
+    ), res.err
+    assert res.out == ""
+
+
+def test_save(capsys: pytest.CaptureFixture, fs: FakeFilesystem):
+    with patch.object(sys, "argv", ["casement", "save"]):
+        with patch.dict(os.environ, {"HOME": "/home/bar"}, clear=True):
+            run(MockI3())
+
+    config_file = fs.get_object(path)
+    config = yaml.safe_load(config_file.contents)
+    assert config == {"outputs": ["foo"], "workspaces": {1: "foo", 2: "foo"}}
+
+    res = capsys.readouterr()
+    assert res.err == ""
+    assert res.out == f"Saved to {path}\n"
+
+
+def test_load_nothing(capsys: pytest.CaptureFixture, fs: FakeFilesystem):
+    with patch.object(sys, "argv", ["casement", "load"]):
+        with patch.dict(os.environ, {"HOME": "/home/bar"}, clear=True):
+            with pytest.raises(SystemExit):
+                run(MockI3())
+
+    res = capsys.readouterr()
+    assert res.err == ""
+    assert res.out == "No existing config for foo\n"
+
+
+def test_load_identical(capsys: pytest.CaptureFixture, fs: FakeFilesystem):
+    fs.create_file(
+        path, contents=yaml.dump({"outputs": ["foo"], "workspaces": {1: "foo"}})
+    )
+
+    with patch.object(sys, "argv", ["casement", "load"]):
+        with patch.dict(os.environ, {"HOME": "/home/bar"}, clear=True):
+            run(MockI3())
+
+    res = capsys.readouterr()
+    assert res.err == ""
+    assert res.out == "No changes necessary\n"
+
+
+def test_load_different(capsys: pytest.CaptureFixture, fs: FakeFilesystem):
+    fs.create_file(
+        path, contents=yaml.dump({"outputs": ["foo"], "workspaces": {1: "bar"}})
+    )
+
+    mock_i3 = MockI3()
+    with patch.object(sys, "argv", ["casement", "load"]):
+        with patch.dict(os.environ, {"HOME": "/home/bar"}, clear=True):
+            run(mock_i3)
+
+    res = capsys.readouterr()
+    assert res.err == ""
+    assert res.out == "Workspaces reset\n"
+
+    assert mock_i3.commands_run == [
+        "workspace 1",
+        "move workspace to output bar",
+        "workspace 2",
+        "workspace 1",
+    ]
+
+
+def test_load_failure(capsys: pytest.CaptureFixture, fs: FakeFilesystem):
+    fs.create_file(
+        path, contents=yaml.dump({"outputs": ["foo"], "workspaces": {1: "bar"}})
+    )
+
+    class FailingMockI3(MockI3):
+        def command(self, cmd: str) -> List[CommandReply]:
+            super().command(cmd)
+            return [CommandReply(success=False, error="some error")]
+
+    mock_i3 = FailingMockI3()
+    with patch.object(sys, "argv", ["casement", "load"]):
+        with patch.dict(os.environ, {"HOME": "/home/bar"}, clear=True):
+            with pytest.raises(SystemExit):
+                run(mock_i3)
+
+    res = capsys.readouterr()
+    assert res.err == ""
+    assert res.out == "Failed to execute 'workspace 1': some error\n"
+
+    assert mock_i3.commands_run == ["workspace 1"]
