f65dcf685aa53de373f0b7f774f3537cc811ee43
Tom Parker-Shemilt <palfrey@tevp.net>
2023-05-29 16:45:08 +0100

    Add pre-commit

--- casement.py
+++ casement.py
@@ -1,9 +1,10 @@
 import hashlib
-from pathlib import Path
-from i3ipc import Connection
 from argparse import ArgumentParser
-import yaml
+from pathlib import Path
 from sys import exit
+
+import yaml
+from i3ipc import Connection
 
 i3 = Connection()
 workspaces = i3.get_workspaces()
@@ -16,22 +17,26 @@ subparsers.add_parser("load")
 
 ns = parser.parse_args()
 
-output_names = sorted([output.name for output in outputs if not output.name.startswith("xroot")])
+output_names = sorted(
+    [output.name for output in outputs if not output.name.startswith("xroot")]
+)
 hasher = hashlib.sha256()
 hasher.update(";".join(output_names).encode("utf-8"))
 key = hasher.hexdigest()
 path = Path(f"~/.config/casement/{key}.yaml").expanduser()
 path.parent.mkdir(parents=True, exist_ok=True)
 
+
 def check_command(command: str):
     rets = i3.command(command)
     for ret in rets:
         if not ret.success:
             print(f"Failed to execute '{command}': {ret.error}")
-            exit(-1)    
+            exit(-1)
+
 
 if ns.subparser == "save":
-    data = {"outputs": output_names, "workspaces": {}}        
+    data = {"outputs": output_names, "workspaces": {}}
 
     for workspace in workspaces:
         data["workspaces"][workspace.num] = workspace.output
@@ -49,16 +54,21 @@ elif ns.subparser == "load":
         exit(-1)
     with path.open() as f:
         config = yaml.safe_load(f)
-    
-    existing_workspaces = dict([(workspace.num, workspace.output) for workspace in workspaces])
-    focused_workspace = [workspace.num for workspace in workspaces if workspace.focused][0]
-    visible_workspaces = [workspace.num for workspace in workspaces if workspace.visible]
+
+    existing_workspaces = dict(
+        [(workspace.num, workspace.output) for workspace in workspaces]
+    )
+    focused_workspace = [
+        workspace.num for workspace in workspaces if workspace.focused
+    ][0]
+    visible_workspaces = [
+        workspace.num for workspace in workspaces if workspace.visible
+    ]
     changes = False
     for name, output in config["workspaces"].items():
         if existing_workspaces[name] != output:
             changes = True
-            for command in [
-                f"workspace {name}", f"move workspace to output {output}"]:
+            for command in [f"workspace {name}", f"move workspace to output {output}"]:
                 check_command(command)
 
     if changes:
